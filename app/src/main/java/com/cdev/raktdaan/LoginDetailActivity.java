6119b5d2e3a0139afc2a6b683410a785ec776959
Surendra Kumar <sk2812283@gmail.com>
2017-05-25 23:45:53 +0530

    Some changes

--- app/src/main/java/com/cdev/raktdaan/LoginDetailActivity.java
+++ app/src/main/java/com/cdev/raktdaan/LoginDetailActivity.java
@@ -5,13 +5,19 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.cdev.raktdaan.nonactivity.UserDetail;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,18 +31,17 @@ public class LoginDetailActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private Toolbar toolbar;
-    private String[] bloodGroups;
-    private String[] rhFactor;
-    private String[] genders;
+
     private String EmailUser;
+    private String gender = "Male";
 
     //Backend
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
 
-    private Spinner spinnerBloodGroup;
-    private Spinner spinnerRhFactor;
-    private Spinner spinnerGender;
+    private TextView bloodGroup;
+    private RadioGroup mRadioGroup;
+    private RadioButton radioButton1, radioButton2;
     private EditText etDOB;
     private EditText etMobileNumber;
     private EditText etAddress;
@@ -69,9 +74,30 @@ public class LoginDetailActivity extends AppCompatActivity {
             }
         });
 
+        bloodGroup = (TextView) findViewById(R.id.textView_bloodGroup);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup_gender);
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton_male);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton_female);
         etDOB = (EditText) findViewById(R.id.etDOB);
         etMobileNumber = (EditText) findViewById(R.id.etMobileNumber);
         etAddress = (EditText) findViewById(R.id.etAddress);
+
+        bloodGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBloodGroup();
+            }
+        });
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (checkedId == R.id.radioButton_male) {
+                    gender = "Male";
+                } else {
+                    gender = "Female";
+                }
+            }
+        });
 
     }
 
@@ -92,12 +118,12 @@ public class LoginDetailActivity extends AppCompatActivity {
         editor.putBoolean("loginStatus", true);
         editor.commit();
 
-        //BE
-        UserDetail newUser = new UserDetail(spinnerBloodGroup.getSelectedItem().toString()+spinnerRhFactor.getSelectedItem().toString(),
-                spinnerGender.getSelectedItem().toString(),etDOB.getText().toString().trim(),
-                etMobileNumber.getText().toString().trim(),etAddress.getText().toString().trim());
-        mDatabaseReference.child("users").child(EmailUser).child("Details").push().setValue(newUser);
-        mDatabaseReference.child("signedInUsers").child(EmailUser).push().setValue("1");
+//        //BE
+//        UserDetail newUser = new UserDetail(spinnerBloodGroup.getSelectedItem().toString()+spinnerRhFactor.getSelectedItem().toString(),
+//                spinnerGender.getSelectedItem().toString(),etDOB.getText().toString().trim(),
+//                etMobileNumber.getText().toString().trim(),etAddress.getText().toString().trim());
+//        mDatabaseReference.child("users").child(EmailUser).child("Details").push().setValue(newUser);
+//        mDatabaseReference.child("signedInUsers").child(EmailUser).push().setValue("1");
         Intent intent = new Intent(LoginDetailActivity.this, HomeActivity.class);
         startActivityForResult(intent, GO_LOGIN_TO_HOME);
     }
@@ -109,5 +135,45 @@ public class LoginDetailActivity extends AppCompatActivity {
             setResult(resultCode);
             finish();
         }
+    }
+
+    public void showBloodGroup() {
+
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.blood_group_dialog, null);
+        builder.setView(view);
+        builder.setTitle("Blood Group");
+        final android.support.v7.app.AlertDialog dialog = builder.create();
+
+        TextView b1 = (TextView) view.findViewById(R.id.button1);
+        TextView b2 = (TextView) view.findViewById(R.id.button2);
+
+        final NumberPicker np = (NumberPicker) view.findViewById(R.id.numberPicker1);
+        np.setMinValue(0);
+        np.setMaxValue(3);
+        final String[] bgArr = new String[]{"A", "B", "O", "AB"};
+        np.setDisplayedValues(bgArr);
+        np.setWrapSelectorWheel(false);
+        final NumberPicker np2 = (NumberPicker) view.findViewById(R.id.numberPicker2);
+        np2.setMaxValue(1);
+        np2.setMinValue(0);
+        final String[] rfArr = new String[]{"+", "-"};
+        np2.setDisplayedValues(rfArr);
+        np2.setWrapSelectorWheel(false);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bloodGroup.setText(bgArr[np.getValue()] + rfArr[np2.getValue()]);
+                dialog.dismiss();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
